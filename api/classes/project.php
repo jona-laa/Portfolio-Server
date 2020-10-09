aa2f68c00ff80b1d9517e2b88e3967639a42ea22
Jonathan Laasonen <iphntm@iPHNTMs-MBP.lan>
2020-10-10 01:03:40 +0200

    add -last endpoint added

---
+++ api/classes/project.php
@@ -0,0 +1,115 @@
+<?php
+class Project {
+    private $conn;
+    private $table_name = "projects";
+  
+    // Job Properties
+    public $id;
+    public $title;
+    public $prj_url;
+    public $descr;
+    public $img_src;
+  
+    public function __construct($db){
+        $this->conn = $db;
+    }
+
+
+
+    // Get All Jobs
+    function read(){
+        $query = "SELECT id, title, prj_url, descr, img_src FROM $this->table_name";
+        $result = $this->conn->prepare($query);
+        $result->execute();
+        return $result;
+    }
+
+
+
+    // Get One Job
+    function readOne($id){
+        $query = "SELECT id, title, prj_url, descr, img_src FROM $this->table_name WHERE id=$id";        
+        $result = $this->conn->prepare($query);
+        $result->execute();
+        return $result;
+    }
+
+
+
+    // Create New Job
+    function create() {
+       $query = "INSERT INTO 
+       $this->table_name
+            SET
+                title=:title, prj_url=:prj_url, descr=:descr, img_src=:img_src";
+  
+        // Prepare query statement
+        $statement = $this->conn->prepare($query);
+    
+        // Sanitize data
+        $this->title=htmlspecialchars(strip_tags($this->title));
+        $this->prj_url=htmlspecialchars(strip_tags($this->prj_url));
+        $this->descr=htmlspecialchars(strip_tags($this->descr));
+        $this->img_src=htmlspecialchars(strip_tags($this->img_src));
+    
+        // Bind values
+        $statement->bindParam(":title", $this->title);
+        $statement->bindParam(":prj_url", $this->prj_url);
+        $statement->bindParam(":descr", $this->descr);
+        $statement->bindParam(":img_src", $this->img_src);
+    
+        if($statement->execute()){
+            return true;
+        }
+    
+        return false;
+    }
+
+
+
+    // Delete Job
+    function delete($id) {
+        $query = "DELETE FROM $this->table_name WHERE id=$id";
+        $result = $this->conn->prepare($query);
+        $result->execute();    
+        return $result;
+    }
+
+
+    
+    // Update Job
+    function update() {
+        $query = "UPDATE 
+            $this->table_name
+                SET
+                    title = :title,
+                    prj_url = :prj_url,
+                    descr = :descr,
+                    img_src = :img_src
+                WHERE
+                    id = :id";
+    
+        $statement = $this->conn->prepare($query);
+    
+        // Sanitize data
+        $this->id=htmlspecialchars(strip_tags($this->id));
+        $this->title=htmlspecialchars(strip_tags($this->title));
+        $this->prj_url=htmlspecialchars(strip_tags($this->prj_url));
+        $this->descr=htmlspecialchars(strip_tags($this->descr));
+        $this->img_src=htmlspecialchars(strip_tags($this->img_src));
+
+        // Bind Values
+        $statement->bindParam(':id', $this->id);
+        $statement->bindParam(':title', $this->title);
+        $statement->bindParam(':prj_url', $this->prj_url);
+        $statement->bindParam(':descr', $this->descr);
+        $statement->bindParam(':img_src', $this->img_src);
+    
+        if($statement->execute()){
+            return true;
+        }
+    
+        return false;
+    }
+}
+?>
---
+++ api/projects.php
@@ -0,0 +1,162 @@
+<?php
+include_once './db/database.php';
+include_once './classes/project.php';
+
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE');
+header('Access-Control-Allow-Headers: Access-Control-Allow-Headers, Content-Type, Access-Control-Allow-Methods, Authorization, X-Requested-With');
+  
+$req_method = $_SERVER['REQUEST_METHOD'];
+
+
+
+// Get value of query parameter id
+if(isset($_GET['id'])) {
+    $id = $_GET['id'];
+}
+
+
+
+// Instantiate DB and project
+$database = new Database();
+$db = $database->getConnection();
+$project = new Project($db);
+
+
+
+/* API Endpoints
+  * @param     {string}     $req_method     Request methos
+*/
+switch($req_method) {
+    
+    // GET
+    case 'GET':
+        // Get all or One project?
+        if(isset($id)) {
+            $result = $project->readOne($id);
+        } else {
+            $result = $project->read();
+        }
+        
+        $rows = $result->rowCount();
+          
+        // If any projects found, Return JSON object
+        if($rows>0){
+        
+            $projects_arr=array();
+            $projects_arr["projects"]=array();
+        
+            while ($row = $result->fetch(PDO::FETCH_ASSOC)){
+                extract($row);
+          
+                $project_item=array(
+                    "id" => $id,
+                    "title" => $title,
+                    "prj_url" => $prj_url,
+                    "descr" => $descr,
+                    "img_src" => $img_src,
+                );
+          
+                array_push($projects_arr["projects"], $project_item);
+            }
+
+            http_response_code(200);
+            echo json_encode($projects_arr);
+
+        } else {
+            http_response_code(404);
+            echo json_encode(
+                array("code" => 404, "message" => "No projects found.")
+            );
+        }
+        break;
+
+        
+        
+    // POST
+    case 'POST':
+        $data = json_decode(file_get_contents("php://input"));
+
+        // Deny req if empty input
+        if(
+            !empty($data->title) &&
+            !empty($data->prj_url) &&
+            !empty($data->descr) &&
+            !empty($data->img_src)
+        ){
+            // set project property values
+            $project->title = $data->title;
+            $project->prj_url = $data->prj_url;
+            $project->descr = $data->descr;
+            $project->img_src = $data->img_src;
+    
+            if($project->create()) {
+                http_response_code(201);
+                    echo json_encode(
+                    array("code" => 201, "message" => "New project created")
+                );
+            } else {
+                http_response_code(503);
+                echo json_encode(
+                    array("code" => 503, "message" => "Something went wrong. Try again.")
+                );
+            }
+        } else{
+            http_response_code(400);        
+            echo json_encode(array("code" => 400, "message" => "Unable to create project. Data is incomplete."));
+        }
+        break;
+    
+    
+    
+    // DELETE
+    case 'DELETE':
+        if(!isset($id)) {
+            http_response_code(510);
+            echo json_encode(
+                array("code" => 510, "message" => "No id was sent")
+            );
+        } else {
+            if($project->delete($id)) {
+                http_response_code(200);
+                echo json_encode(
+                    array("code" => 200, "message" => "project deleted")
+                );
+            } else {
+                http_response_code(503);
+                echo json_encode(
+                    array("code" => 503, "message" => "Sever error. Try again.")
+                );
+            }
+        }
+        break;
+    
+
+
+    // PUT
+    case 'PUT':
+        $data = json_decode(file_get_contents("php://input"));
+
+        // set ID property of project to be edited
+        $project->id = $data->id;
+        
+        // set project property values
+        $project->title = $data->title;
+        $project->prj_url = $data->prj_url;
+        $project->descr = $data->descr;
+        $project->img_src = $data->img_src;
+
+        if($project->update()) {
+            http_response_code(200);
+            echo json_encode(
+                array("code" => 200, "message" => "project updated")
+            );
+        } else {
+            http_response_code(503);
+            echo json_encode(
+                array("code" => 503, "message" => "Sever error. Try again.")
+            );           
+        }
+        break;
+}
